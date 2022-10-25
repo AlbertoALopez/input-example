fa017a73c33cf0d910b8b655e8c2c6ead6a0138d
Alberto Lopez <albertolopez19@gmail.com>
2022-10-25 01:46:02 -0400

    Deploy

---
+++ _next/static/mmo1_YC6N0un5LJbwyRns/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/293134409d0b2f49.css","static/chunks/pages/index-a5fe50217e74b70e.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
